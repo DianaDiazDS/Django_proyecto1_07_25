6e6ccb50682fb894a57f09e565e940d1368b62c9
DianaDiazDS <93956532+DianaDiazDS@users.noreply.github.com>
2025-09-17 20:08:03 -0500

    preuba de ingreso de datos a la base

--- app4/models.py
+++ app4/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Reservation(models.Model):
     name = models.CharField(max_length=100, blank=True)
-    contact = models.CharField('nmumero de telefono',  max_length=300)
+    contact = models.CharField('mumero de telefono',  max_length=300)
     time = models.TimeField()
     count = models.IntegerField()
     notes = models.CharField(max_length=300, blank=True)
